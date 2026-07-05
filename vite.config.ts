35483dddf0e32de4e51d0db3c45d4659027ca881
Ololade <73138104+zololade@users.noreply.github.com>
2026-07-05 18:05:09 +0100

    refactor: clean up initial vite addons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
